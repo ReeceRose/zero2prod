1235ed7d2de9bcf6120856d1d1e4db65fbe95c2a
Reece Rose <reecerose18@gmail.com>
2022-11-19 13:03:16 -0500

    Work on subscriptions route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpListener;
+
+use zero2prod::run;
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("0.0.0.0:8080").expect("Failed to bind 8080 port");
+    run(listener)?.await
 }
